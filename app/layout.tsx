262b963697a8dd9023f728831b676b0e949f30fa
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-06 21:44:23 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import { Header } from '@/lib/header';
 import './globals.css';
 import type { Metadata } from 'next';
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SpeedInsights />
+      <Analytics />
       <body className={inter.className}>
         <BackgroundWallpaper />
         <Header />
