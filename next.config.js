45931e91c5b3f2d974403ff170430c356f534d5f
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-08 21:00:52 +0100

    Redirect old index.php to new home

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     deviceSizes: [430, 768, 1024],
     imageSizes: [430, 676, 1024],
   },
+  async redirects() {
+    return [
+      {
+        // ?p=news%2Fcornelius-t%C3%B8ndel-interview-leatt-rider'
+        source: '/index.php',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
