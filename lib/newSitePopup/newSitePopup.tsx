388f8cdae405c5ba600d219834e45e8b964113c7
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-08 22:13:54 +0100

    Add suspense

--- lib/newSitePopup/newSitePopup.tsx
+++ lib/newSitePopup/newSitePopup.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from 'next/navigation';
 import styles from './newSitePopup.module.css';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 export const NewSitePopup = () => {
   const searchParams = useSearchParams();
@@ -11,26 +12,28 @@ export const NewSitePopup = () => {
   return (
     <>
       {oldSearchParam && (
-        <div className={styles.popupContainer}>
-          <div className={styles.popup}>
-            <p className={styles.popupTitle}>Story not found!</p>
-            <p className={styles.popupDescription}>
-              Unfortunately, the link you clicked doesn&apos;t work anymore, but
-              you might have some luck clicking the button below.
-            </p>
-            <div className={styles.links}>
-              <Link
-                className={styles.primary}
-                href={`/${searchParams.get('p')}`}
-              >
-                Let&apos;s try it
-              </Link>
-              <Link className={styles.secondary} href={'/'}>
-                Go home
-              </Link>
+        <Suspense>
+          <div className={styles.popupContainer}>
+            <div className={styles.popup}>
+              <p className={styles.popupTitle}>Story not found!</p>
+              <p className={styles.popupDescription}>
+                Unfortunately, the link you clicked doesn&apos;t work anymore,
+                but you might have some luck clicking the button below.
+              </p>
+              <div className={styles.links}>
+                <Link
+                  className={styles.primary}
+                  href={`/${searchParams.get('p')}`}
+                >
+                  Let&apos;s try it
+                </Link>
+                <Link className={styles.secondary} href={'/'}>
+                  Go home
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </Suspense>
       )}
     </>
   );
