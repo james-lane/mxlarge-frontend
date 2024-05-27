f223762803ec3eb78cbe0c609f488c1f249b90dc
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-27 22:25:12 +0100

    Bug fix

--- utils/google/sendEvent.tsx
+++ utils/google/sendEvent.tsx
@@ -2,12 +2,15 @@
 
 import { Advert } from '@/lib/types';
 import { sendGAEvent } from '@next/third-parties/google';
+import { useEffect } from 'react';
 
 export const SendAdEvent = ({ ad }: { ad: Advert }) => {
-  sendGAEvent('event', 'advert_loaded', {
-    name: ad.title,
-    size: ad.advertCategory,
-  });
+  useEffect(() => {
+    sendGAEvent('event', 'advert_loaded', {
+      name: ad.title,
+      size: ad.advertCategory,
+    });
+  }, []);
 
   return null;
 };
